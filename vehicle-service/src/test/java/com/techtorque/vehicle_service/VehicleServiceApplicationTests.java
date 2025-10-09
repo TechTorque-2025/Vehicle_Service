84c5c8cb9ad638876551fa4dda6f718160ca7c4c
RandithaK <bharkula3@gmail.com>
2025-10-10 01:30:34 +0530

    Add initial project structure and configuration for vehicle-service microservice

---
+++ vehicle-service/src/test/java/com/techtorque/vehicle_service/VehicleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtorque.vehicle_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
